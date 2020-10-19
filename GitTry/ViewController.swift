a60ec08461154aab677627ad3ec78042563997f5
ningdandan <einooo520@gmail.com>
2020-10-19 17:08:11 -0400

    Initial Commit

---
+++ GitTry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTry
+//
+//  Created by 张薴丹 on 10/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
